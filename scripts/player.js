82c68f02c600b5f1b791ef7aefa11c5a74001423
reboot <djps94@outlook.com>
2024-01-29 11:49:20 +0000

    Prototipo player insert Player

--- scripts/player.js
+++ scripts/player.js
@@ -1,6 +1,36 @@
-function Player(x, y){
+function Player(x, y, parent){
+    var self = this
     this.x = x;
     this.y = y;
     this.directionX = 0;
     this.directionY = 0;
+    this.width = 50;
+    this.height = 50;
+    this.sprite = document.createElement('div')
+
+
+    this.insertPlayer = function() {
+        this.sprite.setAttribute('id', 'player');
+        parent.appendChild(this.sprite)
+        this.sprite.style.left = this.x + 'px'
+        this.sprite.style.top = this.y + 'px'
+    }
+
+    this.movePlayerX = function() {
+        var newX = self.x + 25 * self.directionX
+        if (newX >= 0 && newX <= 500) {
+            self.x = newX
+            self.sprite.style.left = self.x + 'px'
+        }
+    }
+
+    this.movePlayerY = function() {
+        var newY = self.y + 25 * self.directionY
+        if (newY >= 0 && newY <= 500) {
+            self.y = newY
+            self.sprite.style.top = self.y + 'px'
+        }
+    }
 }
+
+export {Player}
--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,35 @@
+import {Player} from './player.js'
+
 var board = document.getElementById("board")
-var player = document.getElementById("player")
+var player = new Player(250, 500, board)
+var playerId = null;
 
-var playerX = 250
-var playerY = 500
-var directionX = 0
-var directionY = 0
-
-function movePlayerX(directionX){
-    playerX += 3 * directionX
-    player.style.left = (playerX) + 'px'
+function gameStart() {
+    player.insertPlayer()
+    playerId = setInterval(movement, 100)
 }
 
-function movePlayerY(directionY){
-    playerY += 3 * directionY
-    player.style.top = (playerY) + 'px'
+function movement() {
+    player.movePlayerX();
+    player.movePlayerY();
 }
 
 window.addEventListener('keydown', function(e){
     switch (e.key) {
         case 'w':
-            movePlayerY('-1')
+            player.directionY = -1
             break;
 
         case 'a':
-            movePlayerX('-1')
+            player.directionX = -1
             break;
         
         case 's':
-            movePlayerY('1')
+            player.directionY = 1
             break;
 
         case 'd':
-            movePlayerX('1')
+            player.directionX = 1
             break;
 
         default:
@@ -41,6 +39,8 @@ window.addEventListener('keydown', function(e){
 
 window.addEventListener('keyup', function()
 {
-    directionX = 0
-    directionY = 0
+    player.directionX = 0
+    player.directionY = 0
 })
+
+gameStart();
