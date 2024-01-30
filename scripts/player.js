9853738680f375edca53fd79199db2fb422005b8
reboot <djps94@outlook.com>
2024-01-30 12:02:13 +0000

    Rana sigue muriendo tronco :)

--- scripts/player.js
+++ scripts/player.js
@@ -1,4 +1,4 @@
-function Player(x, y, parent){
+function Player(x, y, parent, logs){
     var self = this
     this.x = x;
     this.y = y;
@@ -19,7 +19,7 @@ function Player(x, y, parent){
     }
 
     this.movePlayerX = function() {
-        var newX = self.x + 10 * self.directionX
+        var newX = self.x + 50 * self.directionX
         if (newX >= 0 && newX <= 500) {
             self.x = newX
             self.sprite.style.left = self.x + 'px'
@@ -27,7 +27,7 @@ function Player(x, y, parent){
     }
 
     this.movePlayerY = function() {
-        var newY = self.y + 10 * self.directionY
+        var newY = self.y + 50 * self.directionY
         if (newY >= 0 && newY <= 500) {
             self.y = newY
             self.sprite.style.top = self.y + 'px'
@@ -35,6 +35,16 @@ function Player(x, y, parent){
         if (self.y === 0) {
             console.log("Ganaste");
             self.won = true;
+        }
+    }
+    
+    this.dead = function () {
+        if (self.y <= 200 && self.y >= 100) {
+            if(logs.some(log => log.isInLog())){
+                self.isDead = false;
+            } else {
+                self.isDead = true;
+            }
         }
     }
 }
